d30a6849b9babe5be3f545bd7c671edbe2e8db21
Bunny Vishal <vishal@bunnyvishal.com>
2016-10-08 19:50:16 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'port': 3000 || process.env.PORT,
+    'port': process.env.PORT || 3000,
     'db': process.env.DB ||'mongodb://192.168.0.6:27017/picterest',
     'secret': process.env.SECRET || 'Shfs-hO87-Ojd9I-Ejfskj89IkjdP'
 };
